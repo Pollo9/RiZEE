f882e398d0fb25aea745731e0a3bf885d7cded33
Alexandrely <alexandre.ly2@gmail.com>
2018-10-16 00:56:08 +0200

    network manager part 2
    deplacement des joueurs differents + nouveau mode "matchmaker" avec possibilité de crée un host

---
+++ PROJET JEU/Assets/Network/HostGame.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;	
+
+public class HostGame : MonoBehaviour
+{
+
+	[SerializeField] 
+	private uint roomSize = 6;
+
+	private string RoomName;
+	private NetworkManager networkManager;
+
+	public void Start()
+	{
+		networkManager = NetworkManager.singleton;
+		if (networkManager.matchMaker == null)
+		{
+			networkManager.StartMatchMaker();
+		}
+	}
+	public void SetRoomName(string _name)
+	{
+		RoomName = _name;
+	}
+
+	public void CreateRoom()
+	{
+		if (RoomName != "" && RoomName != null)
+		{
+			Debug.Log("Creation de la partie : " + RoomName + " avec " + roomSize + "places.");
+
+			networkManager.matchMaker.CreateMatch(RoomName, roomSize, true, "", "", "", 0, 0,
+				networkManager.OnMatchCreate);
+			//premier "" il s'agit du mdp
+		}
+		
+		//Créer la partie
+		
+	}
+ 	
+	
+
+
+
+}
