99adccd621c3a22c0ad44914e67f068deb9cfdec
theghostgat <theghost663@gmail.com>
2018-11-15 17:39:01 +0100

    ajout porte

---
+++ PROJET JEU/Assets/EffectExamples/ouverture_porte.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class ouverture_porte : MonoBehaviour
+{
+
+	private Animator Anim;
+	
+	void Start ()
+	{
+		Anim = GameObject.Find("glass").GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Anim.SetBool("open",true);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		Anim.SetBool("open",false);
+	}
+
+	private void Update()
+	{
+		
+	}
+}
