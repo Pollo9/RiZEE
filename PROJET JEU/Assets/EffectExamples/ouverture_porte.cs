44f5cd8354ac1f1de475162fbdc6acc2e0159800
theghostgat <theghost663@gmail.com>
2018-11-22 17:26:40 +0100

    Amélioration porte

--- PROJET JEU/Assets/EffectExamples/ouverture_porte.cs
+++ PROJET JEU/Assets/EffectExamples/ouverture_porte.cs
@@ -1,30 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Linq;
+using Boo.Lang;
 using UnityEngine;
 
 public class ouverture_porte : MonoBehaviour
 {
 
 	private Animator Anim;
+	private bool inGUI;
 	
+	
+	
+
 	void Start ()
 	{
 		Anim = GameObject.Find("glass").GetComponent<Animator>();
 	}
+    
+	
+	
+	private void Update()
+	{
 
+		
+	}
+	
 	void OnTriggerEnter(Collider other)
 	{
-		Anim.SetBool("open",true);
+		inGUI = true;
+		Anim.SetBool("open", true);	
 	}
 
 	void OnTriggerExit(Collider other)
 	{
-		Anim.SetBool("open",false);
-	}
-
-	private void Update()
-	{
+		inGUI = false;
 		
 	}
+
+
+	private void OnGUI()
+	{
+		if (inGUI)
+		{ 
+			GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 100, 25), "Open door");	
+		}
+		
+	}
+
+	
 }
