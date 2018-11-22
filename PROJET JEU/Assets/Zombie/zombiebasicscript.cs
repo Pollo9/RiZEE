204fa60c2a8cc136fc8289fca08862b25e0329ae
Truong Anthony <tonyly1@hotmail.fr>
2018-11-22 18:06:44 +0100

    Zombie bot

---
+++ PROJET JEU/Assets/Zombie/zombiebasicscript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class zombiebasicscript : MonoBehaviour
+{
+
+    public Transform player;
+    public List<GameObject> destinationpoints;
+    public float speed;
+    public float alertdistance;
+    public float walkingdistance;
+    public float attackingdistance;
+
+    private Animator anim;
+    private Vector3 direction;
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = false;
+
+    }
+   
+    // Update is called once per frame
+    void Update () {
+        //alert
+        if (Vector3.Distance(player.position, transform.position) < alertdistance &&
+            Vector3.Distance(player.position, transform.position) > walkingdistance)
+        {
+            agent.enabled = false;
+            anim.SetBool("isidle",false);
+            anim.SetBool("isalert", true);
+            anim.SetBool("iswalking",false);
+            anim.SetBool("isattack",false);
+        }
+        //attack & move
+        else if (Vector3.Distance(player.position, transform.position) <= walkingdistance)
+        {
+            agent.enabled = true;
+            agent.SetDestination(player.transform.position);
+            anim.SetBool("isidle",false);
+            anim.SetBool("isalert", false);
+            anim.SetBool("iswalking",true);
+            anim.SetBool("isattack",false);
+            if (Vector3.Distance(player.position, transform.position) <= attackingdistance)
+            {
+                anim.SetBool("iswalking",false);
+                anim.SetBool("isattack",true);
+            }
+        }
+        //idle
+        else if (anim.GetBool("isidle") == false && agent.enabled == false)
+        {
+            anim.SetBool("isidle",true);
+            anim.SetBool("isalert", false);
+            anim.SetBool("iswalking",false);
+            anim.SetBool("isattack",false);
+        }
+    }
+}
