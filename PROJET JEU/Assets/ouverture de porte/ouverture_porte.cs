0e5af60304e1d8ff1cf9ef40bafd70bebb74e1c7
theghostgat <theghost663@gmail.com>
2018-11-22 18:18:20 +0100

    double porte

--- PROJET JEU/Assets/ouverture de porte/ouverture_porte.cs
+++ PROJET JEU/Assets/ouverture de porte/ouverture_porte.cs
@@ -8,29 +8,21 @@ using UnityEngine;
 public class ouverture_porte : MonoBehaviour
 {
 
-	private Animator Anim;
+	private Animator Anim_r;
+	private Animator Anim_l;
 	private bool inGUI;
 	
-	
-	
-
 	void Start ()
 	{
-		Anim = GameObject.Find("glass").GetComponent<Animator>();
+		Anim_r = GameObject.Find("glass_door_left").GetComponent<Animator>();
+		Anim_l = GameObject.Find("glass_door_right").GetComponent<Animator>();
 	}
     
-	
-	
-	private void Update()
-	{
-
-		
-	}
-	
 	void OnTriggerEnter(Collider other)
 	{
 		inGUI = true;
-		Anim.SetBool("open", true);	
+		Anim_r.SetBool("open", true);
+		Anim_l.SetBool("open", true);
 	}
 
 	void OnTriggerExit(Collider other)
@@ -44,10 +36,14 @@ public class ouverture_porte : MonoBehaviour
 	{
 		if (inGUI)
 		{ 
-			GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 100, 25), "Open door");	
+			GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 110, 25), "the door is open");	
 		}
 		
 	}
 
-	
+	private void Update()
+	{
+
+		
+	}
 }
