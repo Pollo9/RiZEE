296eec562777b7ab2225b13aab6938909612016b
Truong Anthony <tonyly1@hotmail.fr>
2018-12-06 18:51:39 +0100

    dialogue1

---
+++ PROJET JEU/Assets/Dialogue/Getkey.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Getkey : MonoBehaviour 
+{
+    private bool inGUI;
+	private bool sortie = true;
+
+    void OnTriggerEnter(Collider other)
+	    {
+	    	inGUI = true;
+	    }
+	void OnTriggerExit(Collider other)
+    	{
+    		inGUI = false;
+		    sortie = false;
+    	}
+    	
+    private void OnGUI()
+        {
+        	if (inGUI && sortie)
+        	{ 
+        		GUI.Box(new Rect(Screen.width / 2, Screen.height / 3, 110, 25), "I got a key !");	
+        	}
+        		
+        }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PROJET JEU/Assets/Dialogue/firstzombie.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstzombie : MonoBehaviour {
+	
+	private bool inGUI;
+	private bool sortie = true;
+
+	void OnTriggerEnter(Collider other)
+	{
+		inGUI = true;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		inGUI = false;
+		sortie = false;
+
+	}
+    	
+	private void OnGUI()
+	{
+		if (inGUI && sortie)
+		{ 
+			GUI.Box(new Rect(Screen.width / 2, Screen.height / 3, 250, 25), "Oh my god... they are here too");	
+		}
+        		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
