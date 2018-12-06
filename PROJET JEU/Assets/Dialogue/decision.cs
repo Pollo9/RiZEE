b062ad0c0cd40aa173d863226c8e4bd5a02bc873
Truong Anthony <tonyly1@hotmail.fr>
2018-12-06 19:34:21 +0100

    dialogue2

---
+++ PROJET JEU/Assets/Dialogue/decision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class decision : MonoBehaviour {
+	
+	private bool inGUI;
+	private bool sortie = true;
+
+	void OnTriggerEnter(Collider other)
+	{
+		inGUI = true;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		inGUI = false;
+		sortie = false;
+
+	}
+    	
+	private void OnGUI()
+	{
+		if (inGUI && sortie)
+		{ 
+			GUI.Box(new Rect(Screen.width / 2, Screen.height / 3, 250, 25), "Ok, how can I open this door...");	
+		}
+        		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PROJET JEU/Assets/Dialogue/passit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class passit : MonoBehaviour {
+	
+	private bool inGUI;
+	private bool sortie = true;
+
+	void OnTriggerEnter(Collider other)
+	{
+		inGUI = true;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		inGUI = false;
+		sortie = false;
+	}
+    	
+	private void OnGUI()
+	{
+		if (inGUI && sortie)
+		{ 
+			GUI.Box(new Rect(Screen.width / 2, Screen.height / 3, 200, 25), "Hum... How to pass this");	
+		}
+        		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
