d57e70f8b2583c352247d2703ce3ef583198d38c
Truong Anthony <tonyly1@hotmail.fr>
2018-12-30 20:21:07 +0100

    zombie

--- PROJET JEU/Assets/Zombie/Zombie_Target.cs
+++ PROJET JEU/Assets/Zombie/Zombie_Target.cs
@@ -1,97 +1,108 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Networking;
 
-public class Zombie_Target : NetworkBehaviour 
- {
-     private Transform target; //the enemy's target
-     private float moveSpeed = 3f; //move speed
-     private float rotationSpeed = 3f; //speed of turning
-     private float range = 20f;
-     private float range2 = 20f;
-     private float stop = 10;
-     private Transform myTransform; //current transform data of this enemy
-     private LayerMask raycastLayer;
- 
-     //Shooting
-     public GameObject shot;
-     public Transform shotSpawn;
-     private int randomCount;
- 
-     void Awake()
-     {
-         myTransform = transform; //cache transform data for easy access/preformance
-     }
-   
-     void Start()
-     {
-         raycastLayer = 1 << LayerMask.NameToLayer ("Player"); //target the player
-     }
-   
-     void FixedUpdate () 
-     {
-         moveToTarget ();
-     }
- 
-     void Update ()
-     {
-         randomCount = Random.Range (1, 100);
-         if (!isServer) 
-             return;
- 
-         var distance2 = Vector3.Distance(myTransform.position, target.position);
-         if (distance2 <= range) 
-         {
-             CmdDoFire ();            
-         }    
-     }
- 
-     void moveToTarget ()
-     {
-         if (!isServer)
-             return;
- 
-         if (target != null && isServer) 
-         {
-             Collider[] distance = Physics.OverlapSphere (myTransform.position, range, raycastLayer);
-             
-             if (distance.Length > 0) 
-             {
-                 int randomint = Random.Range (0, distance.Length);
-                 target = distance[randomint].transform;
-             }
-             
-             var distance1 = Vector3.Distance(myTransform.position, target.position);
-             
-             if (distance1 <= range2 && distance1 >= range) 
-             {
-                 myTransform.rotation = Quaternion.Slerp (myTransform.rotation,
-                  Quaternion.LookRotation (target.position - myTransform.position), rotationSpeed * Time.deltaTime);
-             } 
-             else if (distance1 <= range && distance1 > stop) 
-             { 
-                 //move towards the player
-                 myTransform.rotation = Quaternion.Slerp (myTransform.rotation,
-                 Quaternion.LookRotation (target.position - myTransform.position), rotationSpeed * Time.deltaTime);
-                 myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
-             } 
-             else if (distance1 <= stop) 
-             {
-                 myTransform.rotation = Quaternion.Slerp (myTransform.rotation,
-                  Quaternion.LookRotation (target.position - myTransform.position), rotationSpeed * Time.deltaTime);
-             }
-         }
-     }
- 
-     [Command]
-     void CmdDoFire ()
-     {
-         if (randomCount < 2) 
-         {
-             GameObject instance = (GameObject)Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
-             GetComponent<AudioSource> ().Play ();
-             NetworkServer.Spawn (instance);
-         }
-     }
+public class Zombie_Target : NetworkBehaviour
+{
+	private NavMeshAgent agent;
+    private Transform myTransform;
+    public Transform targetTransform;
+    private LayerMask raycastLayer;
+    private float radius = 100;
+	private Animator anim;
+	public float attackingdistance;
+	public float walkingdistance;
+     
+     	// Use this for initialization
+     	void Start ()
+	     {
+		    anim = GetComponent<Animator>();
+     		agent = GetComponent<NavMeshAgent>();
+     		myTransform = transform;
+     		raycastLayer = 1<<LayerMask.NameToLayer("Player");
+		     agent.enabled = false;
+     
+     		if(isServer)
+     		{
+     			StartCoroutine(DoCheck());
+     		}
+     	}
+     
+     	// Update is called once per frame
+     	void FixedUpdate () 
+     	{
+     		SearchForTarget();
+     		MoveToTarget();
+     	}
+     
+     	void SearchForTarget()
+     	{
+     		if(!isServer)
+     		{
+     			return;
+     		}
+     
+     		if(targetTransform == null)
+     		{
+     			Collider[] hitColliders = Physics.OverlapSphere(myTransform.position, radius, raycastLayer);
+     
+     			if(hitColliders.Length>0)
+     			{
+     				int randomint = Random.Range(0, hitColliders.Length);
+     				targetTransform = hitColliders[randomint].transform;
+				    agent.enabled = false;
+				    anim.SetBool("isidle",false);
+				    anim.SetBool("isalert", true);
+				    anim.SetBool("iswalking",false);
+				    anim.SetBool("isattack",false);
+     			}
+     		}
+		     else
+		     {
+			     if (Vector3.Distance(targetTransform.position, transform.position) <= walkingdistance)
+			     {
+				     agent.enabled = true;
+				     agent.SetDestination(targetTransform.transform.position);
+				     anim.SetBool("isidle",false);
+				     anim.SetBool("isalert", false);
+				     anim.SetBool("iswalking",true);
+				     anim.SetBool("isattack",false);
+				     if (Vector3.Distance(targetTransform.position, transform.position) <= attackingdistance)
+				     {
+					     anim.SetBool("iswalking",false);
+					     anim.SetBool("isattack",true);
+				     }
+			     }
+		     }
+     
+     		if(targetTransform != null && targetTransform.GetComponent<BoxCollider>().enabled == false)
+     		{
+     			targetTransform = null;
+     		}
+     	}
+     
+     	void MoveToTarget()
+     	{
+     		if(targetTransform != null && isServer)
+     		{
+			     SetNavDestination(targetTransform);
+     		}
+     	}
+     
+     	void SetNavDestination(Transform dest)
+     	{
+     		agent.SetDestination(dest.position);
+     	}
+     
+     	IEnumerator DoCheck()
+     	{
+     		for(;;)
+     		{
+     			SearchForTarget();
+     			MoveToTarget();
+     			yield return new WaitForSeconds(0.2f);
+     		}
+     	}
  }
