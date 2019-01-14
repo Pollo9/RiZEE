4fa9612307773783834791b09b27b343cb38a58f
theghostgat <theghost663@gmail.com>
2019-01-14 16:09:05 +0100

    ajout son eau

---
+++ PROJET JEU/Assets/sons/play_water.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play_water : MonoBehaviour
+{
+
+	public AudioSource water;
+		
+	void OnTriggerEnter(Collider other)
+	
+	{
+		water.Play();		
+	}
+}
