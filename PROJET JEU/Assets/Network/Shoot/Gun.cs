1a672d4b06f22a32432b3d5873331d360a3865fe
Pollo9 <43851080+Pollo9@users.noreply.github.com>
2019-01-15 20:00:27 +0100

    alexandre me casse les couilles

---
+++ PROJET JEU/Assets/Network/Shoot/Gun.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpscam;
+
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.Takedamage(damage);
+            }
+        }
+
+    }
+}
