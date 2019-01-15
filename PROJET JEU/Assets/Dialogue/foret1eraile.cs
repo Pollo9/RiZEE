142f698a59abdf6c11c237b31f893c411e3ed2b0
theghostgat <theghost663@gmail.com>
2019-01-15 18:50:26 +0100

    dialogue foret

---
+++ PROJET JEU/Assets/Dialogue/foret1eraile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foret1eraile : MonoBehaviour {
+	
+	private bool inGUI;
+	private bool sortie = true;
+
+	void OnTriggerEnter(Collider other)
+	{
+		inGUI = true;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		inGUI = false;
+		sortie = false;
+	}
+    	
+	private void OnGUI()
+	{
+		if (inGUI && sortie)
+		{ 
+			GUI.Box(new Rect(Screen.width / 2, Screen.height / 3, 300, 25), "What happened here ... I hope someone survived");	
+		}
+        		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
