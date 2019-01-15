3f735d1f8ff7f190e92cd96abf5eabcc627d633e
Pollo9 <43851080+Pollo9@users.noreply.github.com>
2019-01-15 19:15:23 +0100

    trigger

---
+++ PROJET JEU/Assets/Getobject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Getobject : MonoBehaviour {
+
+	public GameObject hacksaw;
+    
+    private void OnTriggerEnter()
+    {
+        
+            hacksaw.SetActive(false);
+        
+    }
+
+}
