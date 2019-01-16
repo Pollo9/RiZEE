375b0f8e307bb58181d4f376bf2ab022aeb4d57a
Alexandrely <alexandre.ly2@gmail.com>
2019-01-16 11:19:32 +0100

    dernier dialogue

---
+++ PROJET JEU/Assets/Dialogue/ss1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ss1 : MonoBehaviour {
+	
+	private bool inGUI;
+	private bool sortie = true;
+
+	void OnTriggerEnter(Collider other)
+	{
+		inGUI = true;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		inGUI = false;
+		sortie = false;
+	}
+    	
+	private void OnGUI()
+	{
+		if (inGUI && sortie)
+		{ 
+			GUI.Box(new Rect(Screen.width / 2, Screen.height / 3, 350, 25), "Ok let's leave this place... where should I go ?");	
+		}
+        		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
